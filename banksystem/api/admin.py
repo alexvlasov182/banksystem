e29955c5e56825941deaded8ce491d3264a01d50
Oleksandr Vlasov <drumlife182@gmail.com>
2023-10-30 09:25:41 +0100

    Added comments to admin.py

--- banksystem/api/admin.py
+++ banksystem/api/admin.py
@@ -1,8 +1,9 @@
-from __future__ import unicode_literals
-
 from django.contrib import admin
 from .models import Branch, Bank
 
 # Register your models here.
+
+# Register the Branch model with the admin site.
 admin.site.register(Branch)
+# Register the Bank model with the admin site.
 admin.site.register(Bank)
