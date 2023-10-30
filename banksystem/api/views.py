cbf6d538de72691fa90ff0dcdfee63881bf098a7
Oleksandr Vlasov <drumlife182@gmail.com>
2023-10-30 09:19:13 +0100

    Adde best practices for Django and REST views.py

--- banksystem/api/views.py
+++ banksystem/api/views.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 from django.http import Http404
 
@@ -7,30 +5,57 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import *
-from .serializers import *
+from .models import Branch, Bank
+from .serializers import BranchSerializer, BankSerializer
 
 
 class BranchesAPIView(generics.ListCreateAPIView):
+    """
+    List and create branches.
+
+    Attributes:
+        queryset: A queryset of all Branch objects.
+        serializer_class: The serializer class for Branch objects.
+    """
+
     queryset = Branch.objects.all()
     serializer_class = BranchSerializer
 
 
 class BranchDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Branch.objects.all()
-    serializer_class = BranchSerializer
+    """
+    Retrive, update, or delete a specific branch.
 
+    Attributes:
+        queryset: A queryset of all Branch objects.
+        serializer_class: The serializer class for Branch objects.
+    """
 
-class BranchDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Branch.objects.all()
     serializer_class = BranchSerializer
 
 
 class BanksAPIView(generics.ListCreateAPIView):
+    """
+    List and create banks.
+
+    Attributes:
+        queryset: A queryset of all Bank objects.
+        serializer_class: The serializer class for Bank objects.
+    """
+
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
 
 
 class BankDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrive, update, or delete a specific bank.
+
+    Attributes:
+        queryset: A queryset of all Bank objects.
+        serializer_class: The serializer class for Bank objects.
+    """
+
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
