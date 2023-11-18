45f84bf841b49ae54f2fd2d8799b2706e482d0e6
Oleksandr Vlasov <drumlife182@gmail.com>
2023-11-18 17:52:25 +0100

    Added the documentation to the test_view.py

--- banksystem/banksystem/tests/test_views.py
+++ banksystem/banksystem/tests/test_views.py
@@ -7,10 +7,7 @@ from banksystem.models import Customer, BankAccount, Transaction
 
 class CustomerModelTest(TestCase):
     def setUp(self):
-        # Create a user for the Customer
         self.user = User.objects.create_user(username="testuser", password="testpass")
-
-        # Create a Customer
         self.customer = Customer.objects.create(
             name="Test Customer",
             user=self.user,
@@ -20,15 +17,16 @@ class CustomerModelTest(TestCase):
         )
 
     def test_customer_str_representation(self):
-        self.assertEqual(str(self.customer), "Test Customer")
+        """
+        Test the string representation of the Customer model.
+        """
+        expected_str = "Test Customer"
+        self.assertEqual(str(self.customer), expected_str)
 
 
 class BankAccountModelTest(TestCase):
     def setUp(self):
-        # Create a user for the Customer
         self.user = User.objects.create_user(username="testuser", password="testpass")
-
-        # Create a Customer
         self.customer = Customer.objects.create(
             name="Test Customer",
             user=self.user,
@@ -36,24 +34,21 @@ class BankAccountModelTest(TestCase):
             phone_number="1234567890",
             initial_balance=100.00,
         )
-
-        # Create a BankAccount
         self.bank_account = BankAccount.objects.create(
             customer=self.customer, account_number="123456789", balance=50.00
         )
 
     def test_bank_account_str_representation(self):
+        """
+        Test the string representation of the BankAccount model.
+        """
         expected_str = f"Account 123456789 - Test Customer"
         self.assertEqual(str(self.bank_account), expected_str)
 
 
 class TransactionModelTest(TestCase):
     def setUp(self):
-        # Create a user for the Customer
-
         self.user = User.objects.create_user(username="testuser", password="testpass")
-
-        # Create a Customer
         self.customer = Customer.objects.create(
             name="Test Customer",
             user=self.user,
@@ -61,20 +56,18 @@ class TransactionModelTest(TestCase):
             phone_number="1234567890",
             initial_balance=100.00,
         )
-
-        # Create a BankAccount
         self.bank_account = BankAccount.objects.create(
             customer=self.customer, account_number="123456789", balance=50.00
         )
-
-        # Create a Transaction
         self.transaction = Transaction.objects.create(
             bank_account=self.bank_account, transaction_type="Deposit", amount=25.00
         )
 
     def test_transaction_str_representation(self):
+        """
+        Test the string representation of the Transaction model.
+        """
         # Create a Transaction with a specific amount
-
         amount = Decimal("25.00")
         transaction = Transaction.objects.create(
             bank_account=self.bank_account, transaction_type="Deposit", amount=amount
